591882d70ab92d52bbc1c5ce43570d0309651a66
Your Name <ariyaniphone14@gmail.com>
2025-02-14 18:20:22 +0600

    added validations

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ import { useEffect } from "react";
 function App() {
   const [products, setProducts] = useState([]);
 
-  const [currentPage,setCurrentPage]=useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
   const pageSize = 10;
   const totalPages = products.length;
   const noOfPages = Math.ceil(totalPages / pageSize);
-
 
   useEffect(() => {
     fetch(`https://dummyjson.com/products?limit=100`)
@@ -17,24 +16,32 @@ function App() {
       .then((data) => setProducts(data.products));
   }, []);
 
-  const start=currentPage*pageSize;
-  const end=start+pageSize;
+  const start = currentPage * pageSize;
+  const end = start + pageSize;
 
-  const handlePrev=()=>{
-    setCurrentPage(currentPage-1)
-  }
-  const handleNext=()=>{
-    setCurrentPage(currentPage+1)
-  }
+  const handlePrev = () => {
+    setCurrentPage(currentPage - 1);
+  };
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
   return (
     <>
       <div>
-        <button onClick={handlePrev}>Prev</button>
-        {[...Array(pageSize).keys()].map(i=> <button onClick={()=>setCurrentPage(i)} className="button" key={i}>{i}</button>)}
-        <button onClick={handleNext}>Next</button>
-        </div>
-      {products.slice(start,end).map((product) => (
+        <button disabled={currentPage === 0} onClick={handlePrev}>
+          Prev
+        </button>
+        {[...Array(pageSize).keys()].map((i) => (
+          <button onClick={() => setCurrentPage(i)} className="button" key={i}>
+            {i}
+          </button>
+        ))}
+        <button disabled={currentPage === pageSize - 1} onClick={handleNext}>
+          Next
+        </button>
+      </div>
+      {products.slice(start, end).map((product) => (
         <div className="card" key={product.id}>
           <img src={product.thumbnail} alt="" />
           <h1>{product.title}</h1>
