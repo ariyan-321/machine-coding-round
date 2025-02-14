821cbf0ce082e275e067384d215184a65cb1eb17
Your Name <ariyaniphone14@gmail.com>
2025-02-14 17:42:15 +0600

    implimented the pagination

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import { useEffect } from "react";
 function App() {
   const [products, setProducts] = useState([]);
 
+  const [currentPage,setCurrentPage]=useState(0);
   const pageSize = 10;
   const totalPages = products.length;
   const noOfPages = Math.ceil(totalPages / pageSize);
+
 
   useEffect(() => {
     fetch(`https://dummyjson.com/products?limit=100`)
@@ -15,10 +17,13 @@ function App() {
       .then((data) => setProducts(data.products));
   }, []);
 
+  const start=currentPage*pageSize;
+  const end=start+pageSize;
+
   return (
     <>
-      <div>{[...Array(pageSize).keys()].map(i=> <button key={i}>{i}</button>)}</div>
-      {products.map((product) => (
+      <div>{[...Array(pageSize).keys()].map(i=> <button onClick={()=>setCurrentPage(i)} className="button" key={i}>{i}</button>)}</div>
+      {products.slice(start,end).map((product) => (
         <div className="card" key={product.id}>
           <img src={product.thumbnail} alt="" />
           <h1>{product.title}</h1>
