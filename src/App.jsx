9163eaa814e2d92f37e976e4ab4213a085d9dfba
Your Name <ariyaniphone14@gmail.com>
2025-02-14 17:35:43 +0600

    added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-
-import { useState } from 'react'
-import './App.css'
-import { useEffect } from 'react';
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
 
-  const[products,setProducts]=useState([]);
+  const pageSize = 10;
+  const totalPages = products.length;
+  const noOfPages = Math.ceil(totalPages / pageSize);
 
-  const pageSize=10;
-
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`https://dummyjson.com/products?limit=100`)
-    .then(res=>res.json())
-    .then(data=>setProducts(data.products))
-  },[]);
-
-
-
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products));
+  }, []);
 
   return (
     <>
-    {
-      products.map(product=> <div className='card' key={product.id}>
-      <img  src={product.thumbnail} alt="" />
-      <h1>{product.title}</h1>
-      </div>)
-    }
+      <div>{[...Array(pageSize).keys()].map(i=> <button key={i}>{i}</button>)}</div>
+      {products.map((product) => (
+        <div className="card" key={product.id}>
+          <img src={product.thumbnail} alt="" />
+          <h1>{product.title}</h1>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
