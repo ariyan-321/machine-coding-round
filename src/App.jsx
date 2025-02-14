fd98e587c662d1c0443f8fb99f105dcb5f0a81be
Your Name <ariyaniphone14@gmail.com>
2025-02-14 17:30:27 +0600

    added some stylings

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,24 @@ function App() {
 
   const[products,setProducts]=useState([]);
 
+  const pageSize=10;
+
   useEffect(()=>{
-    fetch(`https://dummyjson.com/products`)
+    fetch(`https://dummyjson.com/products?limit=100`)
     .then(res=>res.json())
     .then(data=>setProducts(data.products))
-  },[])
+  },[]);
+
+
 
 
   return (
     <>
     {
-      products.map(product=> <div key={product.id}>{product.title}</div>)
+      products.map(product=> <div className='card' key={product.id}>
+      <img  src={product.thumbnail} alt="" />
+      <h1>{product.title}</h1>
+      </div>)
     }
     </>
   )
